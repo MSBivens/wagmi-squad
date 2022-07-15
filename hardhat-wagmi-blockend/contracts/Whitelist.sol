09793da463be0212044fd206425b88301cf55202
MSBivens <mikebiv95@gmail.com>
2022-07-15 12:34:08 -0500

    natspec update

--- hardhat-wagmi-blockend/contracts/Whitelist.sol
+++ hardhat-wagmi-blockend/contracts/Whitelist.sol
@@ -1,15 +1,20 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 
+/** 
+    @title A restricted whitelist
+    @author Mike Bivens
+    @notice Addresses can only be added to this whitelist by the owner of the contract
+*/
 contract Whitelist {
-    // The owner of the contract
+    // @notice The owner of the contract
     address owner;
 
-    // Stores the whitelisted addresses.
-    // Creates a mapping that receives the user's address and returns if they are whitelisted or not.
+    // @notice Stores the whitelisted addresses.
+    // @dev Creates a mapping that receives the user's address and returns if they are whitelisted or not.
     mapping(address => bool) whitelistedAddresses;
 
-    // Validate only the owner can call the function
+    // @notice Validate only the owner can call the function
     modifier onlyOwner() {
         require(
             msg.sender == owner,
@@ -18,59 +23,59 @@ contract Whitelist {
         _;
     }
 
-    // Adds an address to the whitelist
+    // @notice Adds an address to the whitelist
     function addUserAddressToWhitelist(address _addressToWhitelist)
         public
         onlyOwner
     {
-        // Checks the caller is not already part of the whitelist.
+        // @notice Checks the caller is not already part of the whitelist.
         require(
             !whitelistedAddresses[_addressToWhitelist],
             "Error: Sender already whitelisted"
         );
 
-        // Set whitelist boolean to true.
-        // Addresses are set to false by default.
+        // @dev Set whitelist boolean to true.
+        // @notice Addresses are set to false by default.
         whitelistedAddresses[_addressToWhitelist] = true;
     }
 
-    // Verifies the users address is on the whitelist
+    // @notice Verifies the users address is on the whitelist
     function verifyUserAddress(address _whitelistedAddress)
         public
         view
         returns (bool)
     {
-        // Verifying if the user has been whitelisted
+        // @notice Verifying if the user has been whitelisted
         bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
         return userIsWhitelisted;
     }
 
-    // Is the user whitelisted?
+    // @notice Checks if the user is whitelisted
     function isWhitelisted(address _whitelistedAddress)
         public
         view
         returns (bool)
     {
-        // Verifying if the user has been whitelisted
+        // @notice Verifying if the user has been whitelisted
         return whitelistedAddresses[_whitelistedAddress];
     }
 
-    // Remove user from whitelist
+    // @notice Remove user from whitelist
     function removeUserAddressFromWhitelist(address _addressToRemove)
         public
         onlyOwner
     {
-        // Validate the caller is already part of the whitelist.
+        // @notice Validate the caller is already part of the whitelist.
         require(
             whitelistedAddresses[_addressToRemove],
             "Error: Sender is not whitelisted"
         );
 
-        // Set whitelist boolean to false.
+        // @notice Set whitelist boolean to false.
         whitelistedAddresses[_addressToRemove] = false;
     }
 
-    // Get the owner of the contract
+    // @notice Get the owner of the contract
     function getOwner() public view returns (address) {
         return owner;
     }
