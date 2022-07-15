5ea4ac9208e0e2ec68b9c4b1f738750bd08d7205
MSBivens <mikebiv95@gmail.com>
2022-07-15 13:50:23 -0500

    setup goerli for testing

--- hardhat-wagmi-blockend/hardhat.config.js
+++ hardhat-wagmi-blockend/hardhat.config.js
@@ -1,4 +1,11 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: API_KEY,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
 };
